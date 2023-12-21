43ca2e38ae26141ffe80e5ea11e9aaf9dabfe044
Jacob Janzen <jjanzen@cscscience.ca>
2023-12-21 14:37:27 -0600

    create type

---
+++ lib/puppet/provider/nfstype.rb
@@ -0,0 +1,153 @@
+Puppet::Type.type(:nfstype).provide(:ruby) do
+  commands :runuser => '/usr/sbin/runuser'
+
+  def create_file(path, manage_as, is_directory)
+    if not is_directory
+      begin
+        runuser(['-u', manage_as, '--', 'touch', path])
+      rescue Puppet::ExecutionFailure => e
+        Puppet.debug("#create_file had an error -> #{e.inspect}")
+        return nil
+      end
+    else
+      begin
+        runuser(['-u', manage_as, '--', 'mkdir', path])
+      rescue Puppet::ExecutionFailure => e
+        Puppet.debug("#create_file had an error -> #{e.inspect}")
+        return nil
+      end
+    end
+  end
+
+  def set_owner(path, manage_as, owner)
+    begin
+      runuser(['-u', manage_as, '--', 'chown', owner, path])
+    rescue Puppet::ExecutionFailure => e
+      Puppet.debug("#set_owner had an error -> #{e.inspect}")
+      return nil
+    end
+  end
+
+  def set_group(path, manage_as, group)
+    begin
+      runuser(['-u', manage_as, '--', 'chown', ":#{group}", path])
+    rescue Puppet::ExecutionFailure => e
+      Puppet.debug("#set_group had an error -> #{e.inspect}")
+      return nil
+    end
+  end
+
+  def set_mode(path, manage_as, mode)
+    begin
+      runuser(['-u', manage_as, '--', 'chmod', mode, path])
+    rescue Puppet::ExecutionFailure => e
+      Puppet.debug("#set_mode had an error -> #{e.inspect}")
+      return nil
+    end
+  end
+
+  def file_exists(path, manage_as, is_directory)
+    if not is_directory
+      begin
+        runuser(['-u', manage_as, '--', 'test', '-f', path])
+      rescue Puppet::ExecutionFailure
+        return false
+      end
+    else
+      begin
+        runuser(['-u', manage_as, '--', 'test', '-d', path])
+      rescue Puppet::ExecutionFailure
+        return false
+      end
+    end
+    true
+  end
+
+  def remove_file(path, manage_as)
+    begin
+      runuser(['-u', manage_as, '--', 'rm', '-rf', path])
+    rescue Puppet::ExecutionFailure
+      return nil
+    end
+  end
+
+  def create_helper(path, manage_as, directory, owner, group, mode)
+    # create the file/directory
+    return nil if create_file(path, manage_as, directory) == nil
+
+    # set owner
+    if owner != nil
+      return nil if set_owner(path, manage_as, owner) == nil
+    end
+
+    # set group
+    if group != nil
+      return nil if set_group(path, manage_as, group) == nil
+    end
+
+    # set mode
+    if mode != nil
+      return nil if set_mode(mode, manage_as, mode) == nil
+    end
+  end
+
+  def exists?
+    file_exists(resource[:path], resource[:manage_as], resource[:directory])
+  end
+
+  def destroy
+    remove_file(path, manage_as)
+  end
+
+  def create
+    create_helper(resource[:path], resource[:manage_as], resource[:directory], resource[:owner], resource[:group], resource[:mode])
+  end
+
+  def directory
+    file_exists(resource[:path], resource[:manage_as], true)
+  end
+
+  def directory=(value)
+    remove_file(resource[:path], resource[:manage_as])
+    create_helper(resource[:path], resource[:manage_as], resource[:directory], resource[:owner], resource[:group], resource[:mode])
+  end
+
+  def owner
+    begin
+      user = runuser(['-u', manage_as, '--', 'stat', '--printf=%U', path])
+    rescue Puppet::ExecutionFailure
+      return nil
+    end
+    user
+  end
+
+  def owner=(value)
+    set_owner(resource[:path], resource[:manage_as], resource[:owner])
+  end
+
+  def group
+    begin
+      grp = runuser(['-u', manage_as, '--', 'stat', '--printf=%G', path])
+    rescue Puppet::ExecutionFailure
+      return nil
+    end
+    grp
+  end
+
+  def group=(value)
+    set_owner(resource[:path], resource[:manage_as], resource[:group])
+  end
+
+  def mode
+    begin
+      md = runuser(['-u', manage_as, '--', 'stat', '--printf=0%a', path])
+    rescue Puppet::ExecutionFailure
+      return nil
+    end
+    md
+  end
+
+  def mode=(value)
+    set_owner(resource[:path], resource[:manage_as], resource[:mode])
+  end
+end
---
+++ lib/puppet/type/nfstype.rb
@@ -0,0 +1,31 @@
+Puppet::Type.newtype(:nfstype) do
+  desc "Puppet type that manages files like the `file` type but can do so as a specific user"
+
+  ensurable
+
+  newparam(:path, :namevar => :true) do
+    desc "The path to the file to manage. Must be fully qualified."
+  end
+
+  newproperty(:directory) do
+    desc "Whether this file is a directory or not"
+    newvalues(:true, :false)
+  end
+
+  newproperty(:owner) do
+    desc "The user to whom the file should belong."
+  end
+
+  newproperty(:group) do
+    desc "Which group should own the file."
+  end
+
+  newproperty(:mode) do
+    desc "The desired permissions mode for the file in symbolic or numeric notiation. This value must be specified as a string; do not use un-quoted number so to represent file modes."
+    newvalues(/^0\d{3}/)
+  end
+
+  newparam(:manage_as) do
+    desc "The user whom Puppet should use to manage the file."
+  end
+end
