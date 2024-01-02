94a52dc5b94dbc0b3c2975c35f25bef27f2a49c1
Jacob Janzen <jjanzen@cscscience.ca>
2024-01-02 11:38:07 -0600

    clean up and fix runtime errors

--- lib/puppet/provider/nfsfile/ruby.rb
+++ lib/puppet/provider/nfsfile/ruby.rb
@@ -1,53 +1,44 @@
 Puppet::Type.type(:nfsfile).provide(:ruby) do
   commands :runuser => '/usr/sbin/runuser'
 
+  def create_dir(path, manage_as)
+    runuser(['-u', manage_as, '--', 'mkdir', path])
+  rescue Puppet::ExecutionFailure => e
+    Puppet.debug("#create_file had an error -> #{e.inspect}")
+  end
+
   def create_file(path, manage_as, is_directory)
-    if not is_directory
+    if !is_directory
       begin
         runuser(['-u', manage_as, '--', 'touch', path])
       rescue Puppet::ExecutionFailure => e
         Puppet.debug("#create_file had an error -> #{e.inspect}")
-        return nil
       end
     else
-      begin
-        runuser(['-u', manage_as, '--', 'mkdir', path])
-      rescue Puppet::ExecutionFailure => e
-        Puppet.debug("#create_file had an error -> #{e.inspect}")
-        return nil
-      end
+      create_dir(path, manage_as)
     end
   end
 
   def set_owner(path, manage_as, owner)
-    begin
-      runuser(['-u', manage_as, '--', 'chown', owner, path])
-    rescue Puppet::ExecutionFailure => e
-      Puppet.debug("#set_owner had an error -> #{e.inspect}")
-      return nil
-    end
+    runuser(['-u', manage_as, '--', 'chown', owner, path])
+  rescue Puppet::ExecutionFailure => e
+    Puppet.debug("#set_owner had an error -> #{e.inspect}")
   end
 
   def set_group(path, manage_as, group)
-    begin
-      runuser(['-u', manage_as, '--', 'chown', ":#{group}", path])
-    rescue Puppet::ExecutionFailure => e
-      Puppet.debug("#set_group had an error -> #{e.inspect}")
-      return nil
-    end
+    runuser(['-u', manage_as, '--', 'chown', ":#{group}", path])
+  rescue Puppet::ExecutionFailure => e
+    Puppet.debug("#set_group had an error -> #{e.inspect}")
   end
 
   def set_mode(path, manage_as, mode)
-    begin
-      runuser(['-u', manage_as, '--', 'chmod', mode, path])
-    rescue Puppet::ExecutionFailure => e
-      Puppet.debug("#set_mode had an error -> #{e.inspect}")
-      return nil
-    end
+    runuser(['-u', manage_as, '--', 'chmod', mode, path])
+  rescue Puppet::ExecutionFailure => e
+    Puppet.debug("#set_mode had an error -> #{e.inspect}")
   end
 
   def file_exists(path, manage_as, is_directory)
-    if not is_directory
+    if !is_directory
       begin
         runuser(['-u', manage_as, '--', 'test', '-f', path])
       rescue Puppet::ExecutionFailure
@@ -64,31 +55,16 @@ Puppet::Type.type(:nfsfile).provide(:ruby) do
   end
 
   def remove_file(path, manage_as)
-    begin
-      runuser(['-u', manage_as, '--', 'rm', '-rf', path])
-    rescue Puppet::ExecutionFailure
-      return nil
-    end
+    runuser(['-u', manage_as, '--', 'rm', '-rf', path])
+  rescue Puppet::ExecutionFailure
+    nil
   end
 
   def create_helper(path, manage_as, directory, owner, group, mode)
-    # create the file/directory
-    return nil if create_file(path, manage_as, directory) == nil
-
-    # set owner
-    if owner != nil
-      return nil if set_owner(path, manage_as, owner) == nil
-    end
-
-    # set group
-    if group != nil
-      return nil if set_group(path, manage_as, group) == nil
-    end
-
-    # set mode
-    if mode != nil
-      return nil if set_mode(mode, manage_as, mode) == nil
-    end
+    create_file(path, manage_as, directory)
+    set_owner(path, manage_as, owner) unless owner.nil?
+    set_group(path, manage_as, group) unless group.nil?
+    set_mode(mode, manage_as, mode) unless mode.nil?
   end
 
   def exists?
@@ -100,16 +76,22 @@ Puppet::Type.type(:nfsfile).provide(:ruby) do
   end
 
   def create
-    create_helper(resource[:path], resource[:manage_as], resource[:directory], resource[:owner], resource[:group], resource[:mode])
+    create_helper(resource[:path],
+                  resource[:manage_as],
+                  resource[:directory],
+                  resource[:owner],
+                  resource[:group],
+                  resource[:mode])
+  end
+
+  def directory
+    file_exists(resource[:path], resource[:manage_as], true) ? 'true' : 'false'
   end
 
   def owner
-    begin
-      user = runuser(['-u', manage_as, '--', 'stat', '--printf=%U', path])
-    rescue Puppet::ExecutionFailure
-      return nil
-    end
-    user
+    runuser(['-u', resource[:manage_as], '--', 'stat', '--printf=%U', resource[:path]])
+  rescue Puppet::ExecutionFailure
+    nil
   end
 
   def owner=(value)
@@ -117,12 +99,9 @@ Puppet::Type.type(:nfsfile).provide(:ruby) do
   end
 
   def group
-    begin
-      grp = runuser(['-u', manage_as, '--', 'stat', '--printf=%G', path])
-    rescue Puppet::ExecutionFailure
-      return nil
-    end
-    grp
+    runuser(['-u', resource[:manage_as], '--', 'stat', '--printf=%G', resource[:path]])
+  rescue Puppet::ExecutionFailure
+    nil
   end
 
   def group=(value)
@@ -130,12 +109,9 @@ Puppet::Type.type(:nfsfile).provide(:ruby) do
   end
 
   def mode
-    begin
-      md = runuser(['-u', manage_as, '--', 'stat', '--printf=0%a', path])
-    rescue Puppet::ExecutionFailure
-      return nil
-    end
-    md
+    runuser(['-u', resource[:manage_as], '--', 'stat', '--printf=0%a', resource[:path]])
+  rescue Puppet::ExecutionFailure
+    nil
   end
 
   def mode=(value)
